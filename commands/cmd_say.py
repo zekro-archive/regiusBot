bbc5952f244c31eb74b44a63156c3828da56a532
zekroTJA <zekrommaster110@aol.de>
2017-07-02 11:12:44 +0200

    hotfixed say command

--- commands/cmd_say.py
+++ commands/cmd_say.py
@@ -8,3 +8,4 @@ def ex(message, client):
     msg = message.content.replace(STATICS.PREFIX + "say ", "")
 
     yield from client.send_message(message.channel, msg)
+    yield from client.delete_message(message)
