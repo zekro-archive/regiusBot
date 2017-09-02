1d582c084201d6a956e0e418b2a0ee6d46be991f
ゼクロ <zekrommaster110@aol.de>
2017-09-03 01:10:59 +0200

    added stuff

--- utils/gspread_api.py
+++ utils/gspread_api.py
@@ -6,6 +6,30 @@ scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('gsecrets.json', scope)
 
 
+class Settings:
+    def __init__(self, table, sheet):
+        self.gc = gspread.authorize(credentials)
+        self.t = self.gc.open(table).get_worksheet(sheet)
+
+    def get_cval(self, x, y):
+        return self.t.cell(y, x).value
+
+    def set_cval(self, x, y, val):
+        self.t.update_cell(y, x, val)
+
+    def get_next_row(self):
+        count = 1
+        while len(self.t.row_values(count)[0]) > 0:
+            count += 1
+        return count
+
+    def append(self, values):
+        for i, v in enumerate(values):
+            self.t.update_cell(self.get_next_row(), i + 1, v)
+
+    def get_val()
+
+
 def get_stats():
     gc = gspread.authorize(credentials)
     t = gc.open("dd_stats").sheet1
