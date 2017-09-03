463747b0864e2789deee8ba3ef5888c81e71b50f
ゼクロ <zekrommaster110@aol.de>
2017-09-03 02:58:07 +0200

    added gif command

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from discord import Game
 import STATICS
 from commands import cmd_start, cmd_restart, cmd_invite, cmd_google, cmd_log, cmd_dev, cmd_test, cmd_prefix, cmd_dnd, \
     cmd_github, cmd_say, cmd_pmbc, cmd_mute, cmd_xp, cmd_blacklist, cmd_stream, cmd_info, cmd_video, cmd_botkick, \
-    cmd_stats, cmd_user, cmd_exec, cmd_botmsg, cmd_update
+    cmd_stats, cmd_user, cmd_exec, cmd_botmsg, cmd_update, cmd_gif
 from utils import functions, level_system, statistics, userbots, report, perms, rolechange
 
 
@@ -53,6 +53,7 @@ cmdmap = {
         "exec": cmd_exec,
         "botmsg": cmd_botmsg,
         "update": cmd_update,
+        "gif": cmd_gif,
 }
 
 
